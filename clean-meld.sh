c374a0786aa2c7d84aa5f90855e4d8e0b0b0bec7
Plugaru Tudor <plugaru.tudor@gmail.com>
2019-04-09 16:19:35 +0300

    Add meld related script

---
+++ clean-meld.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ${HOME}
+sudo rm -rf ./.local/share/meld
+sudo rm -f ./Library/Preferences/org.gnome.meld.plist
+sudo rm -rf "./Library/Saved Application State/org.gnome.meld.savedState/"
